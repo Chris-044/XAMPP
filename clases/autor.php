90115722d45afa62caaa91db3d2fe6cee067c99c
Chris04 <christiansoberanis208@gmail.com>
2024-05-20 11:09:07 -0600

    CLASS

---
+++ clases/autor.php
@@ -0,0 +1,13 @@
+<?php
+class Autor {
+private $nombre;
+private $apellido;
+private $nacionalidad;
+
+public function __construct(, $nombre, $apellido, $nacionalidad) {
+  $this->nombre = $nombre;
+  $this->apellido = $apellido;
+  $this->nacionalidad = $nacionalidad;
+}
+}
+?>
